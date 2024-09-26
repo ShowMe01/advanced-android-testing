2c8266513e9177d31b7ef1fe4c7d1d921f7aa5ed
ding.feng <ding.feng@hellogroup.com>
2024-09-26 17:26:16 +0800

    finish codelab 3

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -0,0 +1,148 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import android.util.Log
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import com.example.android.architecture.blueprints.todoapp.util.DataBindingIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.monitorActivity
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.core.IsNot.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.math.log
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class TasksActivityTest {
+
+    private val TAG = "TasksActivityTest"
+    private lateinit var repository: TasksRepository
+
+    // An idling resource that waits for Data Binding to have no pending bindings.
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @Before
+    fun init() {
+        Log.d(TAG, "init: ${this}")
+        repository = ServiceLocator.provideTasksRepository(getApplicationContext())
+        runBlocking {
+            repository.deleteAllTasks()
+        }
+    }
+
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @After
+    fun reset() {
+        Log.d(TAG, "reset: ${this}")
+
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun editTask() = runBlocking {
+        Log.d(TAG, "editTask: ${this@TasksActivityTest}")
+
+        // Set initial state.
+        repository.saveTask(Task("TITLE1", "DESCRIPTION"))
+
+        // Start up Tasks screen.
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario) // LOOK HERE
+
+
+        // Espresso code will go here.
+        // Click on the task on the list and verify that all the data is correct.
+        onView(withText("TITLE1")).perform(click())
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("DESCRIPTION")))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
+
+        // Click on the edit button, edit, and save.
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("NEW TITLE"))
+        onView(withId(R.id.add_task_description_edit_text)).perform(replaceText("NEW DESCRIPTION"))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        // Verify task is displayed on screen in the task list.
+        onView(withText("NEW TITLE")).check(matches(isDisplayed()))
+        // Verify previous task is not displayed.
+        onView(withText("TITLE1")).check(doesNotExist())
+
+        // Make sure the activity is closed before resetting the db:
+        activityScenario.close()
+    }
+
+    @Test
+    fun createOneTask_deleteTask() {
+
+        // 1. Start TasksActivity.
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario) // LOOK HERE
+
+        // 2. Add an active task by clicking on the FAB and saving a new task.
+        onView(withId(R.id.add_task_fab)).perform(click())
+        val title = "new title"
+        onView(withId(R.id.add_task_title_edit_text)).perform(typeText(title))
+
+        onView(withId(R.id.add_task_description_edit_text)).perform(
+            typeText("new desc"),
+            closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        // 3. Open the new task in a details view.
+        onView(withText(title)).perform(click())
+
+        // 4. Click delete task in menu.
+        onView(withId(R.id.menu_delete)).perform(click())
+
+        // 5. Verify it was deleted.
+        onView(withId(R.id.menu_filter)).perform(click())
+        onView(withText(R.string.nav_all)).perform(click())
+        onView(withText(title)).check(doesNotExist())
+
+        // 6. Make sure the activity is closed.
+        activityScenario.close()
+
+    }
+
+}
